0884a0d788dc157244c60758c9d4f237847eb690
CHRISTIAN HILL <christian-hill-116@users.noreply.github.com>
2020-01-01 14:52:02 -0700

    init

---
+++ beanCount.js
@@ -0,0 +1,25 @@
+function countBs(string){
+  let count = 0;
+  for(let i = 0; i < string.length; i++){
+    if (string[i] === "B"){
+      count++;
+    }
+    }
+  return count;
+}
+
+function countChar(string, letter){
+  let count = 0;
+  for(let i = 0; i < string.length; i++){
+    if (string[i] === letter){
+      count++;
+    }
+    }
+  return count;
+}
+
+
+console.log(countBs("Bacon"));
+// → 2
+console.log(countChar("kakkerlak", "a"));
+// → 4
